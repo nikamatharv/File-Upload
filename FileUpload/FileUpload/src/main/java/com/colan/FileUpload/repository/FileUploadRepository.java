f9423663b0f0b7d043c374a7882f160f75ac3199
nikamatharv <132284889+nikamatharv@users.noreply.github.com>
2025-01-14 10:31:48 +0530

    Delete FileUpload/FileUpload directory

--- FileUpload/FileUpload/src/main/java/com/colan/FileUpload/repository/FileUploadRepository.java
+++
@@ -1,7 +0,0 @@
-package com.colan.FileUpload.repository; // Correct package
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.colan.FileUpload.model.FileUpload;
-
-public interface FileUploadRepository extends JpaRepository<FileUpload, Long> {
-}
